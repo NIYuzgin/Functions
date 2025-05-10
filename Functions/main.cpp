e39ab49414147c490b08d91628620ce2aec63081
Николай Немов <prost91@outlook.com>
2025-05-10 09:13:19 +0300

    Add Factorial & Power

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,18 +2,20 @@
 
 using namespace std;
 
-// using std::cout;
-
+/*
 int Sum(int a, int b);
 int Difference(int a, int b);
 int Product(int a, int b);
 double Quotient(int a, int b);
+*/
 
+int Factorial (int n);
+double Power(double a, int m);
 
-void main()
-{
+void main() {
 	setlocale(LC_ALL, "");
 
+	/*
 	int a, b;
 	cout << "Введите два числа:"; cin >> a >> b; 
 	int c = Sum (a, b); // Использование функции (Вызов функции - Function call)
@@ -23,8 +25,21 @@ void main()
 	cout << a << " * " << b << " = " << Product(a, b) << endl;
 	cout << a << " / " << b << " = " << Quotient(a, b) << endl;
 
-	}
+	
+	*/
+	
+	int n, m;
+	double a;
+	
+	cout << "Введите число: "; cin >> n;
+	cout << "Факториал числа: " << Factorial(n) << endl;
+	cout << endl;
 
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите степень числа: "; cin >> m;
+	cout << "Степень числа: " << Power(a, m) << endl;
+}
+/*
 int Sum(int a, int b) {
 	int c = a + b;
 	return c;
@@ -41,6 +56,33 @@ int Product(int a, int b) {
 double Quotient(int a, int b) {
 	return (double)a / b;
 }
+*/
+
+int Factorial(int n) {
+	int f = 1;
+	for (int i = 1; i <= n; i++) {
+	f *= i;
+	}
+return f;
+}
+
+double Power(double a, int m) {
+	double N = 1;
+	if (m < 0) {
+		a = 1 / a;
+		m = -m;
+	}
+	for (int i = 0; i < m; i++) {
+	N *= a;
+	}
+return N;
+}
 
 
-//cout << "Hello Functions!" << endl;
+
+
+
+
+
+
+
